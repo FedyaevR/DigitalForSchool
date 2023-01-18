b4d488b9bfd780210b78dd73312fef09639f8774
fedyaevr <dupont6348@gmail.com>
2023-01-18 13:23:39 +0700

    fix

---
+++ DigitalForSchool/wwwroot/js/addStudent.js
@@ -0,0 +1,13 @@
+﻿//$("document").ready(function () {
+
+   
+
+    //for (var i = 0; i < ; i++) {
+
+    //}
+
+})
+//$("#addStudents").on("click", function () {
+//    var studNum = $(this).closest("#numberOfStudents")
+//    alert(studNum);
+//})
